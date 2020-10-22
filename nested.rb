752f2fe5c295172015c6d013c6b4e56b880e7a6b
Nhon Dang <dang.nhon12345@yahoo.com>
2020-10-22 21:52:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -115,7 +115,9 @@ def changing_alan
       }
     }
 
-    programmer_hash[:alan_kay][:known_for] = "GUI"
+alans_new_info = "GUI"
+
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
 
     programmer_hash
 
